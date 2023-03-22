15f54d22536237b7596190e2a814a3f08996d0cc
Boris Goncharov <goncharov.boris@physics.msu.ru>
2023-03-23 03:01:15 +1100

    GWFish pipeline clean up

--- gwfish_analysis/gwfish_memory_pipeline.py
+++ gwfish_analysis/gwfish_memory_pipeline.py
@@ -23,10 +23,6 @@ def powerset(length):
 
 opts = gu.parse_commandline()
 
-#pop_file = '../GWFish/injections/BBH_1e5.hdf5'
-#pop_file = '../GWFish/injections/CBC_pop.hdf5'
-#pop_file = opts.injfile
-
 popdir, totaldir, namebase = gu.output_names(opts)
 
 if not os.path.exists(totaldir):
@@ -35,55 +31,16 @@ if not os.path.exists(totaldir):
 
 threshold_SNR = np.array([0., 9.])  # [min. individual SNR to be included in PE, min. network SNR for detection]
 
-duty_cycle = False                  # whether to consider the duty cycle of detectors
-
 fisher_parameters = opts.fisher_pars.split(',')
 
 parameters = pd.read_hdf(opts.injfile)
 
-#parameters.geocent_time = 0.0
-#parameters.psi = 0.
-#logging.warning('Hard-setting GW phase to pi/2, to avoid discrepancies between TD and SPH waveforms. Phase is not incorporated in TD waveforms, can be fixed in the future.')
-#parameters.phase = np.pi/2 #+ np.pi/12 # TD waveforms are without phase, whereas phase is included for SPH waveforms. This hardcoding makes SPH and TD waveforms consistent
-#parameters['a_1'] = np.repeat(0.1, len(parameters))
-#parameters['a_2'] = np.repeat(0.1, len(parameters))
 parameters['J_E'] = np.repeat(opts.j_e, len(parameters))
 parameters['J_J'] = np.repeat(opts.j_j, len(parameters))
 
-#parameters.luminosity_distance = 3000
-
-# For m1,2 to M,q
-#mass_chirp = gu.m_chirp_from_component_m(parameters.mass_1, parameters.mass_2)
-#mass_ratio = parameters.mass_1/parameters.mass_2
-#parameters = parameters.rename(columns={'mass_1': 'mass_chirp', 'mass_2': 'mass_ratio'})
-#parameters.mass_chirp = mass_chirp
-#parameters.mass_ratio = mass_ratio
-
-# lisaGWresponse(network.detectors[0], frequencyvector)
-# exit()
-
-# horizon(network, parameters.iloc[0], frequencyvector, threshold_SNR, 1./df, fmax)
-# exit()
-
-colors = ['red', 'green', 'blue', 'purple', 'orange', 'brown']
-linestyles = ['-','--',':', '-','--',':']
-#network = {}
 np.random.seed(0)
 
-# Obsolete
-time_domain = [False, True]
-#sph_modes = [False, False]
-
 waveform_class = eval(opts.waveform_class)
-
-print('Processing CBC population')
-
-#waves = {}
-#errors = {}
-#fig, axs = plt.subplots(2,4, figsize=(20, 10), dpi=80)
-
-
-#for wm, wc, co, ls in zip(waveform_models, waveform_class, colors, linestyles):
 
 for kk in range(opts.inj*opts.num, (opts.inj+1)*opts.num):
 
@@ -97,10 +54,8 @@ for kk in range(opts.inj*opts.num, (opts.inj+1)*opts.num):
     parameter_values = parameters.iloc[kk]
     print(parameter_values)
 
-    network = gw.detection.Network([opts.det], detection_SNR=threshold_SNR, parameters=parameters,
-                               fisher_parameters=fisher_parameters, config=opts.config)
+    network = gw.detection.Network([opts.det], detection_SNR=threshold_SNR, parameters=parameters, fisher_parameters=fisher_parameters, config=opts.config)
 
-    #networkSNR_sq = 0
     for dd in np.arange(len(network.detectors)):
 
         data_params = {
@@ -112,8 +67,6 @@ for kk in range(opts.inj*opts.num, (opts.inj+1)*opts.num):
                       }
 
         waveform_obj = waveform_class(opts.waveform, parameter_values, data_params)
-
-        #print('Processing', wm, '. Mismatch with IMRPhenomXPHM (real, imag): ', mismatch(waves[wm],waves[ref_model]).real, mismatch(waves[wm],waves[ref_model]).imag)
 
         network.detectors[dd].fisher_matrix[kk, :, :] = np.zeros((len(fisher_parameters),len(fisher_parameters)))
 
